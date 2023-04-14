26b7c985dc40a20dd2b8a12419229f67ff760271
javedsaqib94 <javed.saqib94@gmail.com>
2023-04-14 21:31:01 +1000

    code added

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:16
+
+# Create app directory 
+WORKDIR /usr/src/app
+
+# Install dependencies
+# 
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 8080
+
+CMD [ "node", "server.js" ]
+
---
+++ server.js
@@ -0,0 +1,14 @@
+const express =  require('express');
+ 
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello there app2 is here for testing')
+});
+
+
+app.listen(PORT,HOST, ()=>{
+    console.log('It is running on http://' +PORT +':'+HOST)
+});
